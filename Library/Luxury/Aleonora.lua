508b6055f53aa676a6ff08f46336751942602c04
Lorvernyr <loxerya@gmail.com>
2024-11-05 20:02:33 +0700

    Update Aleonora.lua

--- Library/Luxury/Aleonora.lua
+++ Library/Luxury/Aleonora.lua
@@ -4,4 +4,6 @@ if _G.Version == "Thailand" or _G.Version == "Thai" then
 elseif _G.Version == "English" or _G.Version == "Eng" then
     warn("[ENGLISH VERSION]")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/REALXLOXERYY/main/Library/Luxury/Raphael/International.zen"))()
+else
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/REALXLOXERYY/main/Library/Luxury/Raphael/International.zen"))() 
 end
