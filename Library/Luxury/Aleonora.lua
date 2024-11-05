038aa915bcbd22fce73c0bac8737abd6c517a111
Lorvernyr <loxerya@gmail.com>
2024-11-05 19:53:33 +0700

    Update Aleonora.lua

--- Library/Luxury/Aleonora.lua
+++ Library/Luxury/Aleonora.lua
@@ -1,7 +1,7 @@
 if _G.Version == "Thailand" or _G.Version == "Thai" then
     warn("[THAI VERSION]")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/REALXLOXERYY/main/Library/Luxury/Raphael/Thailand.zen"))()
-else
+elseif _G.Version == "English" or _G.Version == "Eng" then
     warn("[ENGLISH VERSION]")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/REALXLOXERYY/main/Library/Luxury/Raphael/International.zen"))()
 end
