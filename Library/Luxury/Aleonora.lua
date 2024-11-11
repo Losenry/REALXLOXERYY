3145ce8a15d7a259ea4f1a6515bf73adb2fc3d59
Lorvernyr <loxerya@gmail.com>
2024-11-11 18:33:52 +0700

    Update Aleonora.lua

--- Library/Luxury/Aleonora.lua
+++ Library/Luxury/Aleonora.lua
@@ -1,11 +1,11 @@
-if _G.Version == nil or _G.Version == "" then
-    _G.Version = "English"
+if _G.Scripts_Language == nil or _G.Scripts_Language == "" then
+    _G.Scripts_Language = "English"
 end
 wait(2)
-if _G.Version == "Thailand" or _G.Version == "Thai" then
+if _G.Scripts_Language == "Thailand" or _G.Scripts_Language == "Thai" then
     warn("[THAI VERSION]")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/REALXLOXERYY/main/Library/Luxury/Raphael/Thailand.zen"))()
-elseif _G.Version == "English" or _G.Version == "Eng" then
+elseif _G.Scripts_Language == "English" or _G.Scripts_Language == "Eng" then
     warn("[ENGLISH VERSION]")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/REALXLOXERYY/main/Library/Luxury/Raphael/International.zen"))()
 else
