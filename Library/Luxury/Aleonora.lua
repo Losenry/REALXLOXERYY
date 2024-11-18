f35aede4b94deddeb1878e7142d3eeb3027286ed
Lorvernyr <loxerya@gmail.com>
2024-11-18 18:18:10 +0700

    Update Aleonora.lua

--- Library/Luxury/Aleonora.lua
+++ Library/Luxury/Aleonora.lua
@@ -1,13 +1,18 @@
 if _G.Scripts_Language == nil or _G.Scripts_Language == "" then
     _G.Scripts_Language = "English"
 end
-wait(2)
-if _G.Scripts_Language == "Thailand" or _G.Scripts_Language == "Thai" then
-    warn("[THAI VERSION]")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/REALXLOXERYY/main/Library/Luxury/Raphael/Thailand.zen"))()
-elseif _G.Scripts_Language == "English" or _G.Scripts_Language == "Eng" then
-    warn("[ENGLISH VERSION]")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/REALXLOXERYY/main/Library/Luxury/Raphael/International.zen"))()
+task.wait(1.5)
+if not shared.BetaMode then
+    if _G.Scripts_Language == "Thailand" or _G.Scripts_Language == "Thai" then
+        warn("[THAI VERSION]")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/REALXLOXERYY/main/Library/Luxury/Raphael/Thailand.zen"))()
+    elseif _G.Scripts_Language == "English" or _G.Scripts_Language == "Eng" then
+        warn("[ENGLISH VERSION]")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/REALXLOXERYY/main/Library/Luxury/Raphael/International.zen"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/REALXLOXERYY/main/Library/Luxury/Raphael/International.zen"))() 
+    end
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/REALXLOXERYY/main/Library/Luxury/Raphael/International.zen"))() 
+    warn("PROJECT 1.5H1 BETA DEVELOPER\n    CLIENT KEYS: ".._G.Key,"\n    ".."CLIENT IDS: ".._G.DiscordId);
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/REALXLOXERYY/refs/heads/main/Library/Luxury/Hinutt3.lua"))();
 end
