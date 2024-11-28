e743afe2cf2227c9bc9ee5749fe1b1b039ada8e4
Lorvernyr <loxerya@gmail.com>
2024-11-28 19:11:50 +0700

    Update Aleonora.lua

--- Library/Luxury/Aleonora.lua
+++ Library/Luxury/Aleonora.lua
@@ -1,22 +1,22 @@
-if not _G.Scripts_Language or _G.Scripts_Language == "" then
-    _G.Scripts_Language = "English"
-end
+local Language = "English"
 
-local function loadScript(url)
-    loadstring(game:HttpGet(url))();
+if _G.Scripts_Language ~= nil then
+    Language = _G.Scripts_Language
+else
+    Language = "English"
 end
 
 local function Loader(language)
     if language:lower():find("thai") then
         print(language)
-        loadScript("https://raw.githubusercontent.com/Losenry/seraph.script/main/Client/LocalScript/Premium/Fisch[2].lua")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/seraph.script/refs/heads/main/Client/LocalScript/Premium/Fisch_Thai.lua"))();
     elseif language:lower():find("english") or language:lower():find("international") then
         print(language)
-        loadScript("https://raw.githubusercontent.com/Losenry/seraph.script/main/Client/LocalScript/Premium/Fisch[1].lua")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/seraph.script/refs/heads/main/Client/LocalScript/Premium/Fisch_English.lua"))();
     else
         warn("Unsupported language. Defaulting to English.")
-        loadScript("https://raw.githubusercontent.com/Losenry/seraph.script/main/Client/LocalScript/Premium/Fisch[1].lua")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/seraph.script/refs/heads/main/Client/LocalScript/Premium/Fisch_English.lua"))();
     end
 end
 
-Loader(_G.Scripts_Language)
+Loader(Language)
