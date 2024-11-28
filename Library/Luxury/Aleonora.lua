adec5600ffde70471e786a710b86ae0a26c1eb4b
Lorvernyr <loxerya@gmail.com>
2024-11-28 19:04:06 +0700

    Update Aleonora.lua

--- Library/Luxury/Aleonora.lua
+++ Library/Luxury/Aleonora.lua
@@ -3,15 +3,15 @@ if not _G.Scripts_Language or _G.Scripts_Language == "" then
 end
 
 local function loadScript(url)
-    if not url then return end
-
     loadstring(game:HttpGet(url))();
 end
 
 local function Loader(language)
     if language:lower():find("thai") then
+        print(language)
         loadScript("https://raw.githubusercontent.com/Losenry/seraph.script/main/Client/LocalScript/Premium/Fisch[2].lua")
     elseif language:lower():find("english") or language:lower():find("international") then
+        print(language)
         loadScript("https://raw.githubusercontent.com/Losenry/seraph.script/main/Client/LocalScript/Premium/Fisch[1].lua")
     else
         warn("Unsupported language. Defaulting to English.")
