ccd487b944cb495e1303ef5c4ea755345612f990
Lorvernyr <loxerya@gmail.com>
2024-12-07 18:01:23 +0700

    Update Aleonora.lua

--- Library/Luxury/Aleonora.lua
+++ Library/Luxury/Aleonora.lua
@@ -1,2 +1,1 @@
-warn("Discontinued")
-while true do setfpscap(1) end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Losenry/seraph.script/refs/heads/main/Client/LocalScript/Premium/Fisch_English.lua"))();
